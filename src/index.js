7d62b5eaf7e74b0715ba15ede15c8b1b059d1644
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-14 01:10:22 +1200

    Set player walking animations

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ class MyGame extends Phaser.Scene {
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
     this.physics.add.collider(this.player, platforms);
+
+    // Animation
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
   }
 
   update() {}
