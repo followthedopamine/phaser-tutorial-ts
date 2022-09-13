8a5a1c5c9cef26daa328ed519fa53aff904b34f6
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-14 01:42:16 +1200

    Set up score mechanics and enemy

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ class MyGame extends Phaser.Scene {
 
     // Set player
     this.player = this.physics.add.sprite(100, 450, "dude");
-    this.player.setBounce(0.2);
+    //this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
     this.physics.add.collider(this.player, platforms);
 
@@ -58,9 +58,62 @@ class MyGame extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
+
+    // Create stars
+    const stars = this.physics.add.group({
+      key: "star",
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+    });
+    stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+    // Stars collision
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(this.player, stars, collect, null, this);
+
+    function collect(player, star) {
+      star.disableBody(true, true);
+      score += 1;
+      scoreText.setText("Score: " + score);
+
+      if (stars.countActive(true) === 0) {
+        stars.children.iterate(function (child) {
+          child.enableBody(true, child.x, 0, true, true);
+        });
+
+        const x =
+          player.x < 400
+            ? Phaser.Math.Between(400, 800)
+            : Phaser.Math.Between(0, 400);
+        const bomb = bombs.create(x, 16, "bomb");
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+      }
+    }
+
+    // Create bombs
+    const bombs = this.physics.add.group();
+    this.physics.add.collider(bombs, platforms);
+    this.physics.add.collider(this.player, bombs, bombTouched, null, this);
+
+    function bombTouched(player, bomb) {
+      this.physics.pause();
+      this.player.setTint(0xff000);
+      this.player.anims.play("turn");
+    }
+
+    // Score text
+    const scoreText = this.add.text(16, 16, "Score: 0", {
+      fontSize: 32,
+      fill: "#000",
+    });
+    let score = 0;
   }
 
   update() {
+    // Set up player movement
     const cursors = this.input.keyboard.createCursorKeys();
     if (cursors.left.isDown) {
       this.player.setVelocityX(-160);
@@ -72,7 +125,7 @@ class MyGame extends Phaser.Scene {
       this.player.setVelocityX(0);
       this.player.anims.play("turn", true);
     }
-
+    // Jump
     if (cursors.up.isDown && this.player.body.touching.down) {
       this.player.setVelocityY(-420);
     }
